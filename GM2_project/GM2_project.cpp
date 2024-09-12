051171027886b489bfa6380eca0d01f7f8bd08d5
mina-401 <dubidubs@naver.com>
2024-09-12 16:35:42 +0900

    초기 커밋.

---
+++ GM2_project/GM2_project.cpp
@@ -0,0 +1,23 @@
+﻿// GM2_project.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Test(int Value0, int Value1, int Value2, int Value3)
+{
+    int* Ptr = &Value0;
+    int TestValue0 = *Ptr;
+
+    int TestValue1 = *(Ptr + 2);
+    int TestValue2 = *(Ptr + 4);
+    int TestValue3 = *(Ptr + 6);
+
+    //int sz = sizeof(Ptr);
+
+    int a = 0;
+}
+
+int main()
+{
+    Test(10, 20, 30, 40);
+}
