1bf049bacc1e489215c1e6ab22feec6e32a48e75
mina-401 <dubidubs@naver.com>
2024-09-23 14:23:13 +0900

    이민하 : strlen 커밋

--- GM2_project/GM2_project.cpp
+++ GM2_project/GM2_project.cpp
@@ -6,8 +6,9 @@
 void Test(int Value0, int Value1, int Value2, int Value3)
 {
     int* Ptr = &Value0;
+    
+    
     int TestValue0 = *Ptr;
-
     int TestValue1 = *(Ptr + 2);
     int TestValue2 = *(Ptr + 4);
     int TestValue3 = *(Ptr + 6);
---
+++ StringCount/StringCount.cpp
@@ -0,0 +1,22 @@
+﻿// StringCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int StringCount(const char* const _str) {
+    
+    int Count = 0;
+    while (_str[Count]) { 
+        Count = Count + 1;
+    }
+
+    return Count;
+}
+int main()
+{
+    
+    int Count = StringCount("BBBB");
+  
+    int a = 0;
+
+}
