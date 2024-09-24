c139138292d5423fe626fda538162e2445244ee6
mina-401 <dubidubs@naver.com>
2024-09-24 14:03:14 +0900

    이민하 : textRpg 커밋

---
+++ TextRpg000/TextRpg000.cpp
@@ -0,0 +1,90 @@
+﻿
+#include <iostream>
+
+const int LINECOUNT = 50;
+
+const int NAMELEN = 10;
+
+
+int PlayerAtt = 0;
+int PlayerHp = 0;
+char PlayerName[NAMELEN] = "NONE";
+
+int MonsterAtt = 10;
+int MonsterHp = 100;
+char MonsterName[NAMELEN] = "NONE";
+
+
+void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
+{
+    for (int i = 0; i < _BufferSize; i += 1)
+    {
+        _Arr[i] = 0;
+    }
+
+    // C스타일 글자 개수 세어주는 함수
+    int Size = strlen(_Name);
+    _Arr[0] = _Name[0];
+
+    for (int i = 0; i < Size; i++)
+    {
+        _Arr[i] = _Name[i];
+    }
+}
+
+// 행동
+void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+{
+    // char[100] = 300번지
+    StrCopy(PlayerName, NAMELEN, _Ptr);
+    PlayerAtt = _Att;
+    PlayerHp = _Hp;
+}
+
+// 모든 문법은 코드를 덜치기 위해서 발전해왔습니다.
+void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+{
+    StrCopy(MonsterName, NAMELEN, _Ptr);
+    MonsterAtt = _Att;
+    MonsterHp = _Hp;
+}
+
+void StatusRender(const char* _Name, int _Att, int _HP)
+{
+    printf_s("%s Status", _Name);
+
+    int nameLineCount = LINECOUNT - strlen(_Name)-strlen(" Status");
+    for (int i = 0; i < nameLineCount; i += 1)
+    {
+        printf_s("-");
+    }
+    printf_s("\n");
+    printf_s("공격력 : %d\n", _Att);
+    printf_s("체력 : %d\n", _HP);
+
+    for (int i = 0; i < LINECOUNT; i += 1)
+    {
+        printf_s("-");
+    }
+    printf_s("\n");
+}
+
+void PlayerStatusRender()
+{
+    StatusRender(PlayerName, PlayerAtt, PlayerHp);
+}
+
+void MonsterStatusRender()
+{
+    StatusRender(MonsterName, MonsterAtt, MonsterHp);
+}
+
+int main()
+{
+    CreatePlayer("mina", 10, 100);
+    CreateMonster("Orc", 10, 50);
+
+    PlayerStatusRender();
+    MonsterStatusRender();
+
+}
